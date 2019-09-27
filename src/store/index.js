11947c610b4a9d9b5cd97d684dd157f5bf27029f
Beomyeon <beomyeon.kim@adriel.ai>
2019-09-27 17:54:06 +0900

    apply review from Adriel

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,8 @@ import 'firebase/firestore'
 
 Vue.use(Vuex)
 
+// 변하지 않는 상수로 활용하는 데이터라면 const를 써주는게 좋을 것 같습니다.
+// 잘 아실것같지만 이러한 파일은 .env에서 관리해서 사용하시는게 좋습니다.
 let firebaseConfig = {
   apiKey: 'AIzaSyA3r63OBSnMXnFsQ466pY5I6nxUtvX8Rtk',
   authDomain: 'adriel-vue.firebaseapp.com',
@@ -24,15 +26,18 @@ export default new Vuex.Store({
     Done: [],
     db: firebase.firestore()
   },
+  // state를 작성하실때 첫문자 대소문자 사용 여부는 통일하시는것이 좋을 것 같습니다.
 
   mutations: {
     addTodoItem (state, todoItem) {
       state[todoItem.type].push(todoItem)
     },
+    // Todo 아이템을 추가할때 소팅처리가 안되어있어서 리스트를 다시 부를때 꼬이는 지점이 있는 것 같습니다. 
     removeTodoItem (state, {type, index}) {
       state[type].splice(index, 1)
     },
   },
+  // Promise를 적절히 잘 사용하신 것 같습니다.
   actions: {
     loadTodos ({ commit, state }) {
       return new Promise(resolve => {
